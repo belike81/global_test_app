c3cd2aecdec8332b35e421353b7bbb8453d20ad2
Tomasz Grabowski <belike81@gmail.com>
2016-09-23 15:44:00 +0200

    Create a location group model

---
+++ db/migrate/20160923134323_create_location_groups.rb
@@ -0,0 +1,9 @@
+class CreateLocationGroups < ActiveRecord::Migration
+  def change
+    create_table :location_groups do |t|
+      t.string :name
+      t.integer :country_id
+      t.integer :panel_provider_id
+    end
+  end
+end
