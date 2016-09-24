69d35be08d671a3ca347e3d337c42db225d7d1ad
Tomasz Grabowski <belike81@gmail.com>
2016-09-24 10:22:45 +0200

    Target groups controller spec update

--- app/models/country.rb
+++ app/models/country.rb
@@ -2,5 +2,5 @@ class Country < ActiveRecord::Base
   belongs_to :panel_provider
   has_one :location_group
   has_many :locations, through: :location_group
-  has_and_belongs_to_many :target_groups, -> { where parent_id: nil }
+  has_and_belongs_to_many :target_groups, -> (country) { where(parent_id: nil, panel_provider_id: country.panel_provider_id) }
 end
--- spec/controllers/api/v1/target_groups_controller_spec.rb
+++ spec/controllers/api/v1/target_groups_controller_spec.rb
@@ -2,20 +2,40 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::TargetGroupsController, type: :controller do
   describe "GET #index" do
-    before(:each) do
-      panel_provider = create(:panel_provider)
-      country = create(:country, panel_provider: panel_provider)
-      @target_group = create(:target_group, countries: [country], panel_provider: panel_provider)
-      get :index, country_code: country.country_code, format: :json
+    context 'correct data provided' do
+      before(:each) do
+        panel_provider = create(:panel_provider)
+        country = create(:country, panel_provider: panel_provider)
+        @target_group = create(:target_group, countries: [country], panel_provider: panel_provider)
+        get :index, country_code: country.country_code, format: :json
+      end
+
+      it "returns a 200 status-code" do
+        expect(response).to be_success
+      end
+
+      it "returns locations belonging to the country based on it's current panel provider" do
+        json = JSON.parse(response.body)
+        expect(json[0]['name']).to eql @target_group.name
+      end
     end
 
-    it "returns a 200 status-code" do
-      expect(response).to be_success
-    end
+    context 'incorrect data provided' do
+      before(:each) do
+        panel_provider = create(:panel_provider)
+        country = create(:country)
+        @target_group = create(:target_group, countries: [country], panel_provider: panel_provider)
+        get :index, country_code: country.country_code, format: :json
+      end
 
-    it "returns locations belonging to the country based on it's current panel provider" do
-      json = JSON.parse(response.body)
-      expect(json[0]['name']).to eql @target_group.name
+      it "returns a 200 status-code" do
+        expect(response).to be_success
+      end
+
+      it "returns locations belonging to the country based on it's current panel provider" do
+        json = JSON.parse(response.body)
+        expect(json).to be_empty
+      end
     end
   end
 end
